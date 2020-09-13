e81b792334d1b361c460a965ce51e990b081a4e6
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-13 19:54:11 +0530

    Conditionals

---
+++ src/introduction/conditionals/Conditionals.java
@@ -0,0 +1,62 @@
+package conditionals;
+
+public class Conditionals {
+  public static void main(String[] args) {
+    int age = 18;
+
+    if (age >= 18) {
+      System.out.println("You can vote!!");
+    }
+
+    int noOfPetals = 21;
+
+    if (noOfPetals % 2 == 0) {
+      System.out.println("I am God!");
+    } else {
+      System.out.println("I am THE God!");
+    }
+
+    int number = 41;
+
+    if (number <= 10) {
+      System.out.println("Less than 10");
+    } else if (number > 10 && number <= 30) {
+      System.out.println("greater than 10 and less than 30");
+    } else if (number > 30 && number <= 40) {
+      System.out.println("greater than 30 and less than 40");
+    } else {
+      System.out.println("greater than 40");
+    }
+
+    int a = 10;
+    int b = 12;
+    int c = 100;
+    int result = -1;
+
+    // Shorthand
+    int maxOfBoth = a > b ? a : b;
+    System.out.println("Max of " + a + " and " + b + ": " + maxOfBoth);
+
+    if (a > b) {
+      if (a > c) {
+        result = a;
+      } else {
+        result = c;
+      }
+    } else {
+      if (b > c) {
+        result = b;
+      } else {
+        result = c;
+      }
+    }
+
+    System.out.println("Max of 3: " + result);
+
+    result = -1;
+
+    // Ternary Magic
+    result = a > b ? a > c ? a : c : b > c ? b : c;
+    System.out.println("Max of 3: " + result);
+  }
+}
