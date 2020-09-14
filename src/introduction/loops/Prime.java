ac423e4f9995a017ac552cf4f2299663e49e4f7b
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-14 20:14:11 +0530

    Prime

---
+++ src/introduction/loops/Prime.java
@@ -0,0 +1,33 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Prime {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    boolean isPrime = true;
+
+    if (n < 2)
+      isPrime = false;
+
+    for (int i = 2; i < n; ++i)
+      if (n % i == 0) {
+        isPrime = false;
+        break;
+      }
+
+    // Optimized - Square root decomposition
+    for (int i = 2; i * i < n; ++i)
+      if (n % i == 0) {
+        isPrime = false;
+        break;
+      }
+
+    System.out.println(" isPrime: " + isPrime);
+
+  }
+
+}
