252a36bf030b3ea2676fed66f8c4d4d12ca8ba99
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-15 20:41:59 +0530

    2 D Arrays

---
+++ src/introduction/arrays/TwoDArray.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class TwoDArray {
+  public static void main(String[] args) {
+    int[][] a = new int[3][5];
+    System.out.println(a[1][4]);
+
+    int[][] b = { { 1, 2, 4, 5 }, { 1, 5 }, { 1, 5, 1 }, };
+    System.out.println(b[1][1]);
+
+  }
+
+}
