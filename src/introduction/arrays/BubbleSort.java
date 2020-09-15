c1731934095d110bae7345e33e791679039e5863
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-15 20:41:42 +0530

    Sorting

---
+++ src/introduction/arrays/BubbleSort.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class BubbleSort {
+  public static void main(String[] args) {
+    int[] a = { 2, 1, 3, -4, 5, 9, 0 };
+    int n = a.length;
+
+    for (int i = 0; i < n - 1; ++i) {
+      boolean sorted = true;
+
+      for (int j = 0; j < n - 1 - i; ++j) {
+        if (a[j + 1] < a[j]) {
+          int temp = a[j];
+          a[j] = a[j + 1];
+          a[j + 1] = temp;
+
+          sorted = false;
+        }
+      }
+
+      if (sorted)
+        break;
+    }
+
+    for (int item : a) {
+      System.out.print(item + " ");
+    }
+  }
+}
---
+++ src/introduction/arrays/SelectionSort.java
@@ -0,0 +1,28 @@
+package arrays;
+
+public class SelectionSort {
+  public static void main(String[] args) {
+    int[] a = { 9, 3, 12, -1, 2, 0 };
+    int n = a.length;
+
+    for (int i = 0; i < n - 1; ++i) {
+      int minIndex = i;
+
+      for (int j = i + 1; j < n; ++j) {
+        if (a[minIndex] > a[j]) {
+          minIndex = j;
+        }
+      }
+
+      if (minIndex != i) {
+        int temp = a[i];
+        a[i] = a[minIndex];
+        a[minIndex] = temp;
+      }
+    }
+
+    for (int item : a)
+      System.out.print(item + " ");
+  }
+
+}
