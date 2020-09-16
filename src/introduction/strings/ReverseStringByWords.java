9221488865f2c128b3531e18ff79af4ced84e83a
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-16 20:44:39 +0530

    Reverse words in string

---
+++ src/introduction/strings/ReverseStringByWords.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class ReverseStringByWords {
+  public static void main(String[] args) {
+
+    String s = " I   Love    Java, the   coffie   ";
+    String[] words = s.trim().split(" ");
+    String reveresedStr = "";
+
+    for (int i = words.length - 1; i >= 0; --i)
+      if (words[i].length() != 0)
+        reveresedStr += words[i] + " ";
+
+    System.out.println(s);
+    System.out.println(reveresedStr);
+
+}
