c60295e416a4a9ca2a6fb35596f39a21c076cab3
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-09-29 22:30:38 +0530

    Check Permutation

---
+++ Java/src/questions/CheckPermutation.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class CheckPermutation {
+
+  static boolean checkPermutation(String s, String t) {
+    if (s.length() != t.length())
+      return false;
+
+    int[] char_set = new int[128];
+
+    char[] s_array = s.toCharArray();
+    for (char c : s_array) {
+      ++char_set[c];
+    }
+
+    for (int i = 0; i < t.length(); ++i) {
+      int val = (int) t.charAt(i);
+
+      --char_set[val];
+
+      if (char_set[val] < 0)
+        return false;
+    }
+
+    return true;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str1 = sc.nextLine();
+    String str2 = sc.nextLine();
+
+    System.out.println("checkPermutation: " + checkPermutation(str1, str2));
+
+  }
+
+}
