ae88440422581a1a5c15178b4c95fbe20a083b94
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-10-05 12:03:27 +0530

    Interface

---
+++ Java/src/oops/interfaces/Person.java
@@ -0,0 +1,33 @@
+package interfaces;
+
+public class Person implements Student, Youtuber {
+
+  @Override
+  public void study() {
+    // TODO Auto-generated method stub
+    System.out.println("Person is studing");
+
+  }
+
+  @Override
+  public void makeVideos() {
+    // TODO Auto-generated method stub
+    System.out.println("Person is making a video");
+  }
+
+  @Override
+  public void editVideo() {
+    // TODO Auto-generated method stub
+
+  }
+
+  public static void main(String[] args) {
+    Person p = new Person();
+
+    p.study();
+    p.makeVideos();
+
+    // Can't instantiate the interface
+
+  }
+}
---
+++ Java/src/oops/interfaces/Youtuber.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public abstract interface Youtuber extends VideoEditor {
+
+  public abstract void makeVideos();
+  
+}
