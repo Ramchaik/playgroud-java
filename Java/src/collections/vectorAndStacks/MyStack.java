ed50fd2cfeb2efb1daefc3b224185a9d57a072a3
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-10-17 18:30:59 +0530

    Create custom stack class

---
+++ Java/src/collections/vectorAndStacks/MyStack.java
@@ -0,0 +1,25 @@
+
+public class MyStack<T> {
+  private MyLinkedList<T> list = new MyLinkedList<>();
+
+  void push(T data) {
+    list.add(data);
+  }
+
+  T pop() throws Exception {
+    if (list.isEmpty()) {
+      throw new Exception("Popping from empty stack");
+    }
+
+    return list.removeLast();
+  }
+
+  T peek() throws Exception {
+    if (list.isEmpty()) {
+      throw new Exception("Popping from empty stack");
+    }
+
+    return list.getLast();
+  }
+  
+}
